190a55d9649cdb52d7a866e84e7d72fc560f66ff
Keli06001 <keli06001@utb.vaxjo.se>
2023-11-29 08:28:35 +0100

    Add project files.

---
+++ Uppgift5.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Inlämningsuppgift5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] heltalArray = new int[3];
+
+            heltalArray[0] = 5;
+            heltalArray[1] = 10;
+
+            Console.Write("Ange det tredje talet: ");
+            heltalArray[2] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"De tre heltalen är: {heltalArray[0]}, {heltalArray[1]}, {heltalArray[2]}");
+
+        }
+    }
+}
